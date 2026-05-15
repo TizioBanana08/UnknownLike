d25d2bf784c86a02b0c21aa7b6903256adc63c81
TizioBanana08 <tommasoruggeri2008@gmail.com>
2026-05-15 20:47:58 +0200

    Add: Working health bars

--- renderer.js
+++ renderer.js
@@ -25,24 +25,47 @@ const giocatore={
 const nemico = {
     nome: "Slime di Codice",
     hp: 80,
+    maxHp:80,
     attacco: 10,
     sprite: "💧"
 };
 function aggiornaUI(){
+    let percentualeVitaP = (giocatore.hp / giocatore.maxHp) * 100;
+    let percentualeVitaE = (nemico.hp / nemico.maxHp) * 100;
     document.getElementById("p-sprite-img").src=giocatore.sprite;
     document.getElementById("p-hp").innerText=giocatore.hp;
     document.getElementById("e-hp").innerText=nemico.hp;
     document.getElementById("p-name").innerText=giocatore.nome;
     document.getElementById("e-name").innerText=nemico.nome;
     document.getElementById("p-weapon").innerText=giocatore.arma.nome;
-
+    cambiaColoreHealthBar(percentualeVitaP,"p-health-bar");
+    cambiaColoreHealthBar(percentualeVitaE,"e-health-bar");
+}
+function cambiaColoreHealthBar(vitaP,id){
+    var progressBar=document.getElementById(id);
+    progressBar.style.width=vitaP+"%";
+    if(vitaP>75){
+        progressBar.style.backgroundColor="#01ad23"
+    }
+    if(vitaP<=75 && vitaP>50){
+        progressBar.style.backgroundColor="#80c02b"
+    }
+    if(vitaP<=50 && vitaP>25){
+        progressBar.style.backgroundColor="#ffd334"
+    }
+    if(vitaP<=25 && vitaP>10){
+        progressBar.style.backgroundColor="#f18030"
+    }
+    if(vitaP<=10){
+        progressBar.style.backgroundColor="#e32f30"
+    }
 }
 function attaccoGiocatore(){
     if(nemico.hp<=0) return;
     nemico.hp-=giocatore.arma.danno;
     aggiungiLog(`${giocatore.nome} usa ${giocatore.arma.nome} e toglie ${giocatore.arma.danno} HP!`);
     if(nemico.hp>0){
-        setTimeout(attaccoNemico,800);
+        setTimeout(attaccoNemico(),800);
     }
     else{
         nemico.hp=0;
