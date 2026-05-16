55e26df7083764b17eceeb2923256c55a7434ade
gioi0 <tolottagioele@gmail.com>
2026-05-16 13:52:06 +0200

    remove: test comment

--- renderer.js
+++ renderer.js
@@ -1,18 +1,3 @@
-
-/*function aggiungiElemento(){
-    const input=document.getElementById("todo-input");
-    const lista=document.getElementById("lista-task");
-    let testo=input.value;
-    if(testo==="") return;
-    let li=document.createElement("li");
-    li.innerHTML=`
-        <span>${testo}</span>
-        <span class="delete-btn" onclick="this.parentElement.remove()">X</span>
-    `;
-    lista.appendChild(li);
-    input.value="";
-}
-*/
 //Oggetto Player
 const giocatore={
     nome:"Cavalier Pristol",
