daea68932749ebc6cf71b1bf1cb317a317c7bdf7
TizioBanana08 <tommasoruggeri2008@gmail.com>
2026-05-20 18:39:55 +0200

    Fix: Merge error fix

--- renderer.js
+++ renderer.js
@@ -303,10 +303,6 @@ function toggleOptions() {
     giocoInPausa = menu.classList.contains("visible");
 }
 
-<<<<<<< Updated upstream
-// --- EVENT LISTENERS E DESCRITTORI OGGETTI ---
-=======
-<<<<<<< HEAD
 function checkStatus(){
     if(giocatore.stato==null){
         giocatore.hp=giocatore.hp;
@@ -314,10 +310,7 @@ function checkStatus(){
         giocatore.hp-=3;
     }
 }
-=======
 // --- EVENT LISTENERS E DESCRITTORI OGGETTI ---
->>>>>>> ca5f311ee6bc39a0a65a69db7d2761cea8a14b87
->>>>>>> Stashed changes
 
 const weaponImg = document.getElementById("weapon-sprite-img");
 const armorImg = document.getElementById("armor-sprite-img");
