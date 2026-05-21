a35da688470a629c3ee54239b26a204bee9a2f61
gioi0 <tolottagioele@gmail.com>
2026-05-21 13:28:20 +0200

    Add: checkstatus function in main

--- renderer.js
+++ renderer.js
@@ -209,6 +209,11 @@ async function attaccoGiocatore() {
         aggiungiLog(`⚔️ ${giocatore.nome} usa ${giocatore.arma.nome} e toglie ${dannoTurno} HP!`);
     }
 
+
+    let danniStato=checkStatus(giocatore.stato);
+    giocatore.hp-=danniStato;
+    aggiornaUI();
+
     if (nemico.hp > 0) {
         gameState.fase = "TURNO_NEMICO";
         await aspetta(1500);
@@ -289,6 +294,9 @@ async function turnoNemico() {
     
     aggiornaUI();
     aggiungiLog(`💥 ${nemico.nome} ti colpisce per ${dannoTurnoNemico} danni!`);
+    let danniStato=checkStatus(nemico.stato);
+    nemico.hp-=danniStato;
+    aggiornaUI();
 
     if (giocatore.hp <= 0) {
         gameState.fase = "GAME_OVER";
@@ -345,11 +353,13 @@ function toggleOptions() {
     }
 }
 
-function checkStatus(){
-    if(giocatore.stato==null){
-        giocatore.hp=giocatore.hp;
-    }else if(giocatore.stato=="burn"){
-        giocatore.hp-=3;
+function checkStatus(stato){
+    if(stato==null){
+        return danni=0;
+    }else if(stato=="burn"){
+        aggiungiLog("danno da bruciatura!")
+        return danno=8;
+
     }
 }
 // --- EVENT LISTENERS E DESCRITTORI OGGETTI ---
